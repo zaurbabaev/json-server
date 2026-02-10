481f17e6dc7c0f4427f4382d0f70871a076dc3c9
Zaur <zaurik87@gmail.com>
2026-02-10 09:31:25 +0400

    update server

--- api/server.js
+++ api/server.js
@@ -1,24 +1,21 @@
-import jsonServer from "json-server";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "../db.json"));
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-
+// Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
   }),
 );
-
 server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
 
-// server.listen() yoxdur, Vercel serverless handle edir
-export default server;
+// Export the Server API
+module.exports = server;
